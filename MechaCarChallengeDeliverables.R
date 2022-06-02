72907db290d53d74b51a2876b6e512dc59fbc36e
Jayendra Vyas <pmj.vyas@gmail.com>
2022-06-02 14:28:12 -0400

    Adding All Challenge file

---
+++ MechaCarChallengeDeliverables.R
@@ -0,0 +1,8 @@
+demo_table <- read.csv(file='demo.csv',check.names=F,stringsAsFactors = F)
+getwd()
+
+C:/Users/pmjvy/OneDrive/Desktop/GW Learnings/Module 15 R
+setwd("C:/Users/pmjvy/OneDrive/Desktop/GW Learnings/Module 15 R/R_Analysis")
+
+demo_table2 <- fromJSON(txt='demo.json')
+getwd()
